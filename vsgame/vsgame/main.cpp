812cda7883c4c78c7b50fd4d8dcc971aff77974a
onoie <onoie3@gmail.com>
2017-09-04 13:12:49 +0900

    add book media

--- vsgame/vsgame/main.cpp
+++ vsgame/vsgame/main.cpp
@@ -4,8 +4,38 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hP, LPSTR lpC ,int nC){
 	if (DxLib_Init() == -1){
 		return 1;
 	}
-	DrawBox(0, 0, 120, 120, 0x00FFF, TRUE);
+	//DrawBox(0, 0, 120, 120, 0x00FFF, TRUE);
+	//WaitKey();
+	//DxLib_End();
+	
+	//int ghandle = LoadGraph("media\\smp1_chara01.png");
+	//DrawGraph(0, 0, ghandle, TRUE);
+	//WaitKey();
+	//DxLib_End();
+
+	int ghandle = LoadGraph("media\\smp1_chara01.png");
+	int x = 0,y=0;
+
+	SetDrawScreen(DX_SCREEN_BACK);
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0){
+		ClearDrawScreen();
+		int key = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+		if (key& PAD_INPUT_UP){ y -= 4; }
+		if (key& PAD_INPUT_DOWN){ y += 4; }
+		if (key& PAD_INPUT_LEFT){ x -= 4; }
+		if (key& PAD_INPUT_RIGHT){ x += 4; }
+		DrawGraph(x,y, ghandle, TRUE);
+		ScreenFlip();
+	}
+	
+	SetDrawScreen(DX_SCREEN_FRONT);
+	ClearDrawScreen();
+	DrawGraph(0, 0, ghandle, TRUE);
+	ScreenFlip();
+
 	WaitKey();
+
 	DxLib_End();
+
 	return 0;
 }
